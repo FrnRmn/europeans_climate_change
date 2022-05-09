54c978366e5b458d32a8f2749a78f4056f266292
Francesco Romandini <francescoromandini@gmail.comgit>
2022-05-09 14:38:55 +0200

    After revision 1

--- weighted_counts.py
+++ weighted_counts.py
@@ -2,6 +2,17 @@ import numpy as np
 import pandas as pd
 
 def weighted_value_counts(df_col,w):
+    '''
+    Count the occurrencies of each unique value in a data column of interest, weighting each observation by a given number
+
+    INPUT
+    df_col: pandas data column containing the values of interest
+    w: array containing oen weight for each observation
+
+    OUTPUT
+    a pandas Series containing the weighted count of unique values in the data column of interest
+    '''
+
     labels = [np.NaN]
     counts = [0]
 
@@ -26,7 +37,21 @@ def weighted_value_counts(df_col,w):
 
     return pd.Series(counts, index=labels, name=df_col.name)
 
+
 def weighted_country_value_counts(df_country_col1, df_col2,w):
+    '''
+    Count the occurrencies of each unique value in a data column of interest, weighting each observation by a given number,
+    dividing the count by country.
+
+    INPUT
+    df_country_col1: pandas data colunb containing a country code for each observation
+    df_col2: pandas data column containing the values of interest
+    w: array containing oen weight for each observation
+
+    OUTPUT
+    a pandas Series containing the weighted count of unique values in the data column of interest diveded by country
+    '''
+
     labels1 = [np.NaN]
     labels2 = [np.NaN]
     counts = [0]
